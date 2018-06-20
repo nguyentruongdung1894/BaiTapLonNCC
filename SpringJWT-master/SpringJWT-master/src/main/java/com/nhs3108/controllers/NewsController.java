bbd70db1cf415a87a4754156b5fb697917fa9364
nguyentruongdung1894 <39787725+nguyentruongdung1894@users.noreply.github.com>
2018-06-20 16:02:02 +0700

    abc

---
+++ SpringJWT-master/SpringJWT-master/src/main/java/com/nhs3108/controllers/NewsController.java
@@ -0,0 +1,20 @@
+package com.nhs3108.controllers;
+
+import com.nhs3108.Utils.FakeUtils;
+import com.nhs3108.models.News;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/news")
+public class NewsController {
+    @RequestMapping(method = RequestMethod.GET)
+
+    public List<News> news(HttpServletRequest request) {
+        return FakeUtils.getAllNews();
+    }
+}
